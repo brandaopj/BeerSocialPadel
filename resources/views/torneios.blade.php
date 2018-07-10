124c8ec1c5d685251ef537559ee3f530c909b2d9
Pedro Brandão <13867808+brandaopj@users.noreply.github.com>
2018-07-10 18:57:06 +0100

    update

--- resources/views/torneios.blade.php
+++ resources/views/torneios.blade.php
@@ -33,27 +33,7 @@
             <BR>
 			<p>Nos torneios PREMIUM / CHALLENGER, as duplas são sorteadas. A definição do sorteio tem por base a classificação de cada jogador no Ranking BSP CS, para definir quais são os cabeças de serie, e a pontuação de cada jogador também serve de critério para distribuir os jogadores pelos 6 campos, pois são realizados simultaneamente dois nonstops de 6 duplas cada.</p>
 			<p>Nos torneios PARTNERS, cada jogador pode escolher o seu parceiro. Neste formato, as duplas são distribuídas pelos 6 campos através da pontuação da dupla, havendo 2  grupos de 6 duplas, jogam entre si em formato non-stop, havendo ainda finalíssima no final, com o 1º do grupo A a defrontar o 1º do grupo B, 2ª vs 2ª, etc.</p>
-            <BR><BR>
-            <H3><div style="color:orange;">BSP PREMIUM</div></H3>
-            <BR>
-            <p>Depois da abertura das inscrições do torneio, nas primeiras 24h, só serão consideradas válidas as inscrições dos jogadores presentes na 1ª metade da Tabela Cabeças de Serie.</p> 
-			<p>Mas todos os jogadores podem inscrever-se e ficam em standby durante 24h, caso não se consiga preencher o torneio com jogadores da zona Premium, as restantes vagas são preenchidas por jogador da zona Challenger, tendo prioridade os jogadores com melhor classificação no ranking.</p>
-            <BR><BR>
-            <H3><div style="color:green;">BSP CHALLENGER</div></H3>
-            <BR>
-            <p>Depois da abertura das inscrições do torneio, nas primeiras 24h, só serão consideradas válidas as inscrições dos jogadores presentes na 2ª metade da Tabela Cabeças de Serie.</p>
-			<p>Mas todos os jogadores podem inscrever-se e ficam em standby durante 24h, caso não se consiga preencher o torneio com jogadores da zona Challenger, as restantes vagas são preenchidas por jogador da zona Premium, tendo prioridade os jogadores com pior classificação no ranking.</p>
-			<p>Os vencedores deste torneio, serão premiados com vaga garantida no torneio BSP PREMIUM após a atualização da tabela.</p>
-            <p>Após esse tempo, todas as inscrições são válidas tendo prioridade os membros com classificação mais baixa até 24h antes do inicio do torneio.</p>
-            <BR><BR>
-            <H3><div style="color:blue;">BSP PARTNERS</div></H3>
-            <BR>
-            <p>Este modelo servirá para que todos os membros possam treinar com o parceiro com que jogam em torneios oficiais.</p>
-			<p>Serão realizados pontualmente e divulgados antecipadamente pela organização.</p>
-			<p>Não há prioridades nas inscrições, nem pontuações atribuídas para as tabelas de classificação geral e cabeças de serie.</p> 
-			<p>Em todos os torneios haverá sempre uma vaga garantida para um fundador BSP.</p>
-			<p>Os fundadores tem o direito a atribuir Wild Cards aos jogadores sempre que achem necessário.</p>
-            <BR><BR>
+           
             
 
         </div>
